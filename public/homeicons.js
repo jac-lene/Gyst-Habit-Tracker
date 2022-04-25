79bd13a995b50f9d1647cd4f9ead22ee452a4b63
jac-lene <jaclenesini@gmail.com>
2022-04-24 23:45:33 -0400

    icon system honed

--- public/homeicons.js
+++ public/homeicons.js
@@ -5,8 +5,11 @@ for (let i = 0; i < activity.length; i++) {
 // const activityArr = activityData.split(',');
 console.log(activityData)
 if (activityData !== null) {
-    console.log('full')
-    document.querySelector('.actIcon').src = "https://i.imgur.com/lwqy3Ee.png"
+    // console.log('full')
+    document.querySelector('.actIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/lwqy3Ee.png" />'
+} else {
+    console.log('empty');
+    document.querySelector('.actIcon').innerHTML = null
 }
 }
 
@@ -17,16 +20,29 @@ for (let i = 0; i < mood.length; i++) {
 // const activityArr = activityData.split(',');
 console.log(moodData)
 if (moodData !== null) {
-    console.log('full')
-    document.querySelector('.moodIcon').src = "https://i.imgur.com/QjBiDfe.png"
+    // console.log('full')
+    document.querySelector('.moodIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/QjBiDfe.png" />'
+} else {
+    console.log('empty');
+    document.querySelector('.moodIcon').innerHTML = null
 }
 }
 
 const thought = document.getElementsByClassName('thoughts')
 
-if (thought !== null) {
+
+for (let i = 0; i < thought.length; i++) {
+    const thoughtData = thought[i].innerHTML;
+const thoughtArr = thoughtData.split('');
+console.log(thoughtArr)
+
+if (thoughtArr.length > null) {
     console.log('full')
-    document.querySelector('.thoughtIcon').src = "https://i.imgur.com/l8WUbhB.png"
+    document.querySelector('.thoughtIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/l8WUbhB.png" />'
+} else {
+    console.log('empty');
+    document.querySelector('.thoughtIcon').innerHTML = null
+}
 }
 
 
