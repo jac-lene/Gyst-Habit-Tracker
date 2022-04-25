923d345c423b85cc06cbeefd739ce68ea1367783
jac-lene <jaclenesini@gmail.com>
2022-04-25 18:11:55 -0400

    got activity icons working, need to fix styling

--- public/homeicons.js
+++ public/homeicons.js
@@ -1,58 +1,62 @@
-// const activity = document.getElementsByClassName('activity')
+const activity = document.getElementsByClassName('activity')
 
-// const entries = document.querySelector('.entry')
+const entries = document.querySelector('.entry')
 
-// // console.log(activity)
+// console.log(activity)
 
-// for (let i = 0; i < activity.length; i++) {
-//     const activityData = activity[i].innerHTML;
-//     // console.log(i)
-// // const activityArr = activityData.split(',');
-// // console.log(activityData)
-// if (activityData !== null) {
-//     // console.log('full')
-//     const newdiv = document.createElement('div')
-//     document.querySelector('.actIcon').appendChild(newdiv)
-//     newdiv.innerHTML = '<img class="actIcon icon" src="https://i.imgur.com/lwqy3Ee.png" />'
-//     // document.querySelector('.actIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/lwqy3Ee.png" />'
-// } else {
-//     console.log('empty');
-//     document.querySelector('.actIcon').innerHTML = null
-// }
-// }
+for (let i = 0; i < activity.length; i++) {
+    const activityData = activity[i].innerHTML;
+    // console.log(i)
+// const activityArr = activityData.split(',');
+// console.log(activityData)
+if (activityData !== null) {
+    // console.log('full')
+    const newdiv = document.createElement('div')
+    document.querySelector('.actIcon').appendChild(newdiv)
+    newdiv.innerHTML = '<img class="actIcon icon" src="https://i.imgur.com/lwqy3Ee.png" />'
+    // document.querySelector('.actIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/lwqy3Ee.png" />'
+} else {
+    console.log('empty');
+    document.querySelector('.actIcon').innerHTML = null
+}
+}
 
-// // const mood = document.getElementsByClassName('mood')
+const mood = document.getElementsByClassName('mood')
 
-// // for (let i = 0; i < mood.length; i++) {
-// //     const moodData = activity[i].innerHTML;
-// // // const activityArr = activityData.split(',');
-// // // console.log(moodData)
-// // if (moodData !== null) {
-// //     // console.log('full')
-// //     document.querySelector('.moodIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/QjBiDfe.png" />'
-// // } else {
-// //     // console.log('empty');
-// //     document.querySelector('.moodIcon').innerHTML = null
-// // }
-// // }
+for (let i = 0; i < mood.length; i++) {
+    const moodData = activity[i].innerHTML;
+// const activityArr = activityData.split(',');
+// console.log(moodData)
+if (moodData !== null) {
+    // console.log('full')
+    const newdiv = document.createElement('div')
+    document.querySelector('.moodIcon').appendChild(newdiv)
+    newdiv.innerHTML = '<img class="moodIcon icon" src="https://i.imgur.com/QjBiDfe.png" />'
+} else {
+    // console.log('empty');
+    document.querySelector('.moodIcon').innerHTML = null
+}
+}
 
-// // const thought = document.getElementsByClassName('thoughts')
-// // console.log(thought)
+const thought = document.getElementsByClassName('thoughts')
+// console.log(thought)
 
-// // for (let i = 0; i < thought.length; i++) {
-// //     console.log(thought[i])
-// //     const thoughtData = thought[i].innerHTML;
-// // const thoughtArr = thoughtData.split('');
+for (let i = 0; i < thought.length; i++) {
+    // console.log(thought[i])
+    const thoughtData = thought[i].innerHTML;
+const thoughtArr = thoughtData.split('');
+console.log(thoughtArr)
 
-
-// // if (thoughtArr.length >= 1) {
-// //     // console.log('full')
-// //     document.querySelector('.thoughtIcon').innerHTML =  '<img class="icon" src="https://i.imgur.com/l8WUbhB.png" />'
-// // } else {
-// //     // console.log('empty');
-// //     document.querySelector('.thoughtIcon').innerHTML = null
-// // }
-// // }
+if (thoughtArr.length >= 1) {
+    // console.log('full')
+    const newdiv = document.createElement('div')
+    document.querySelector('.thoughtIcon').appendChild(newdiv)
+    newdiv.innerHTML = '<img class="thoughtIcon icon" src="https://i.imgur.com/l8WUbhB.png" />'
+} else {
+    // console.log('empty');
+    document.querySelector('.thoughtIcon').innerHTML = null
+}
+}
 
 
 
--- public/showScript.js
+++ public/showScript.js
@@ -1,3 +1,55 @@
+const allActs = document.querySelector('.showAct').innerHTML
+
+const actArr = allActs.split(',')
+
+// console.log(actArr)
+
+const activityMaster = ['social', 'yoga', 'meditation', 'rest', 'exercise', 'dance/play']
+
+function icon(img, name) {
+    const imgDiv = document.createElement('div')
+    const nameDiv = document.createElement('div')
+    document.querySelector('.iconname').appendChild(imgDiv)
+    document.querySelector('.iconname').appendChild(nameDiv)
+    imgDiv.innerHTML = `<img src = "${img}" class="showIcon"/>`
+    nameDiv.innerHTML = `<p>${name}</p>`
+}
+
+
+actArr.forEach((act) => {
+    activityMaster.forEach((activity) => {
+if (act.includes(activity)) {
+    console.log(act)
+    if (act.includes('yoga')) {
+        icon('https://i.imgur.com/e1iwlfc.png', 'Yoga')
+    } 
+    if (act.includes('meditation')) {
+        icon('https://i.imgur.com/PanqHkS.png', 'Meditation')
+    }
+    if (act.includes('social')) {
+        icon('https://i.imgur.com/gPq87BK.png', 'Social')
+    }
+    if (act.includes('rest')) {
+        icon('https://i.imgur.com/yaHCwmH.png', 'Rest')
+    }
+    if (act.includes('exercise')) {
+        icon('https://i.imgur.com/SQHiIVk.png', 'Exercise')
+    }
+    if (act.includes('dance/play')) {
+        icon('https://i.imgur.com/tSyZgXs.png', 'Dance/Play')
+    }
+}
+    }
+)})
+
+
+
+
+
+
+
+
+
 document.getElementById('delete').addEventListener('click', () => {
     console.log('clicked');
     document.querySelector('.modal').style.display = 'block';
