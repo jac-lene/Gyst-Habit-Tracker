4920b1b8a58e69ecddb74d0d736e44dd6b67b02c
jac-lene <jaclenesini@gmail.com>
2022-04-26 13:06:30 -0400

    edit page perfect, need to fix HOME and SHOW

--- public/editScript.js
+++ public/editScript.js
@@ -15,10 +15,26 @@ entryActArr.forEach((enActs) => {
         if (enActs.includes(activities[i].value)) {
             console.log(document.getElementById(activities[i].id))
             document.getElementById(activities[i].id).checked = true;
+            if (document.getElementById(activities[i].id).checked = true) {
+                document.querySelector(`.${activities[i].id}`).style.borderBottom = '1px solid black'
+            }
         }
     } 
 })
 
+const activity = document.querySelectorAll('.activity')
+
+activity.forEach((act) => {
+    // console.log(act.checked)
+    act.addEventListener('click', () => {
+        if (act.checked == true) {
+            // console.log(act.id, ': true')
+            document.querySelector(`.${act.id}`).style.borderBottom = '1px solid black'
+        } else if (act.checked == false) {
+            document.querySelector(`.${act.id}`).style.borderBottom = null
+        }
+        })  
+    })
 
 
 //mood
@@ -29,6 +45,18 @@ const entryMoodArr = entryMoods.split(',')
 
 const moods = document.getElementsByClassName('mood')
 
+const mood = document.querySelectorAll('.mood')
+// console.log(mood)
+
+function checkCheck() {
+    mood.forEach((moocheck) => {
+        if (moocheck.checked == false) {
+            // console.log(moocheck, 'unchecked')
+            document.querySelector(`.${moocheck.id}`).style.borderBottom = null
+        }
+    })
+}
+
 for (let i = 0; i < moods.length; i++) {
     console.log(moods[i].value)
 }
@@ -38,8 +66,23 @@ entryMoodArr.forEach((enMood) => {
         if (enMood.includes(moods[i].value)) {
             console.log(document.getElementById(moods[i].id))
             document.getElementById(moods[i].id).checked = true;
+            if (document.getElementById(moods[i].id).checked = true) {
+                document.querySelector(`.${moods[i].id}`).style.borderBottom = '1px solid black'
+                checkCheck()
+            }
         }
     } 
+})
+
+mood.forEach((moo) => {
+    moo.addEventListener('click', () => {
+        if (moo.checked == true) {
+            // console.log(moo.id, ': true')
+            document.querySelector(`.${moo.id}`).style.borderBottom = '1px solid black'
+            checkCheck()
+            // console.log(mood)
+        }
+    })
 })
 
 //thoughts
